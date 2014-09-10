fdc8f0bc356b00b0548ba1d1386bbce4ce3dd4fc
joyrexus <joyrexus@gmail.com>
2014-09-10 16:13:36 -0500

    add client

--- file-upload/server.js
+++ file-upload/server.js
@@ -6,8 +6,9 @@ server.route({
     method: 'GET',
     path: '/upload',
     handler: function (request, reply) {
-        reply.file('clients/html-form/index.html');
+        // reply.file('clients/html-form/index.html');
         // reply.file('clients/xhr-send/index.html');
+        reply.file('clients/xhr-bootstrap/index.html');
     },
 });
 
