d4e544cd7ab182ae8f4f42f48be71e37e494d098
joyrexus <joyrexus@gmail.com>
2014-11-15 20:38:23 -0600

    add katex demo

---
+++ katex-demo/README.md
@@ -0,0 +1,4 @@
+Quick tire kick of [KaTeX](http://khan.github.io/KaTeX/).
+
+    npm install
+    npm run demo
---
+++ katex-demo/render.js
@@ -0,0 +1,15 @@
+'use strict';
+var fs = require('fs');
+var mustache = require('mustache');
+var katex = require('katex');
+
+var formula = "c = \\pm\\sqrt{a^2 + b^2}";
+var view = { 
+    insert: katex.renderToString(formula)
+};
+
+fs.readFile('index.mustache', 'utf-8', function (err, data) {
+    if (err) { throw err; }
+    var output = mustache.render(data.toString(), view);
+    console.log(output);
+});
